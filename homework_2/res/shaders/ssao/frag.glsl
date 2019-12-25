986a621671083158ac285844b2b08719df20553d
Lazar Jelic <lazarjelic98@gmail.com>
2019-12-25 11:12:14 +0100

    Removed skybox, Removed postprocess, Showing all textures on key press

--- homework_2/res/shaders/ssao/frag.glsl
+++ homework_2/res/shaders/ssao/frag.glsl
@@ -19,8 +19,8 @@ const vec2 noiseScale = vec2(1440.0f / 4.0f, 900.0f / 4.0f);
 void main() {
 	vec3 position = texture(smp_position, pass_uv).xyz;
 	vec3 normal = normalize(texture(smp_normal, pass_uv).xyz);
-	vec3 randomVec = normalize(texture(smp_noise, pass_uv * noiseScale).xyz * vec3(2.0f) + vec3(-1.0f, -1.0f, 0.0f));
-	
+	vec3 randomVec = normalize(texture(smp_noise, pass_uv * noiseScale).xyz * vec3(2.0f) - vec3(1.0f, 1.0f, 0.0f));
+
 	vec3 tangent = normalize(randomVec - normal * dot(randomVec, normal));
 	vec3 bitangent = cross(normal, tangent);
 	mat3 TBN = mat3(tangent, bitangent, normal);
@@ -29,7 +29,7 @@ void main() {
 
 	for(int i = 0; i < kernelSize; i++) {
 		vec2 kernUV = vec2((i / sqrt(kernelSize)) / kernelSize, i % (int(sqrt(kernelSize)) / kernelSize));
-		vec3 kernXYZ = texture(smp_kernel, pass_uv).xyz * vec3(2.0f) + vec3(-1.0f);
+		vec3 kernXYZ = normalize(texture(smp_kernel, pass_uv).xyz * vec3(2.0f, 2.0f, 1.0f) - vec3(1.0f, 1.0f, 0.0f));
 		vec3 kernSample = TBN * kernXYZ;
 		kernSample = position + kernSample * radius;
 	
@@ -39,8 +39,8 @@ void main() {
 		offset.xyz = offset.xyz * 0.5f + 0.5f;
 
 		float kernDepth = texture(smp_position, offset.xy).z;
-
 		float rangeCheck = smoothstep(0.0f, 1.0f, radius / abs(position.z - kernDepth));
+
 		occlusion += (kernDepth >= kernSample.z + bias ? 1.0f : 0.0f) * rangeCheck;
 	}
 
--- homework_2/src/main_state.c
+++ homework_2/src/main_state.c
@@ -6,8 +6,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define TOTAL_STAGES 5
-#define TOTAL_UNIFORMS 10
+#define TOTAL_STAGES 4
+#define TOTAL_UNIFORMS 3
 #define TOTAL_MESHES 1
 #define LENGTH_NAME 128
 #define FIELD_OF_VIEW 75.0f
@@ -18,12 +18,6 @@ static struct pipeline_stage {
 	rafgl_framebuffer_simple_t fbo;
 } stages[TOTAL_STAGES];
 
-static GLuint skybox_shader;
-static GLuint skybox_uni_p, skybox_uni_v;
-
-static rafgl_texture_t skybox_tex;
-static rafgl_meshPUN_t skybox_mesh;
-
 static mat4_t model, view, projection, view_projection;
 
 static vec3_t camera_dir = vec3m(0.0f, 1.0f, 6.5f);
@@ -33,7 +27,7 @@ static vec3_t aim_dir = vec3m(0.0f, 0.0f, -1.0f);
 static rafgl_meshPUN_t meshes[TOTAL_MESHES];
 static const char* mesh_names[TOTAL_MESHES] = { "res/models/monkey.obj" };
 
-static int current_shader = 0;
+static int current_texture = 0;
 static int current_mesh = 0;
 
 static float angle_v = 0.0f;
@@ -51,7 +45,7 @@ static GLuint tex_noise, smp_noise;
 static GLuint tex_kernel, smp_kernel;
 static GLuint tex_ssao, smp_ssao;
 static GLuint tex_blur, smp_blur;
-static GLuint tex_light, smp_light;
+static GLuint tex_light;
 
 static GLuint quadVAO, quadVBO;
 
@@ -89,47 +83,8 @@ void render_quad() {
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
-void init_skybox() {
-	rafgl_texture_load_cubemap_named(&skybox_tex, "above_the_sea", "jpg");
-
-	skybox_shader = rafgl_program_create_from_name("skybox");
-	skybox_uni_v = glGetUniformLocation(skybox_shader, "uni_v");
-	skybox_uni_p = glGetUniformLocation(skybox_shader, "uni_p");
-
-	rafgl_meshPUN_init(&skybox_mesh);
-	rafgl_meshPUN_load_cube(&skybox_mesh, 1.0f);
-}
-
-void render_skybox() {
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glEnable(GL_DEPTH_TEST);
-	glDepthMask(GL_FALSE);
-
-	glUseProgram(skybox_shader);
-	glUniformMatrix4fv(skybox_uni_v, 1, GL_FALSE, (void*) view.m);
-	glUniformMatrix4fv(skybox_uni_p, 1, GL_FALSE, (void*) projection.m);
-
-	glBindVertexArray(skybox_mesh.vao_id);
-
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glEnableVertexAttribArray(2);
-
-	glDrawArrays(GL_TRIANGLES, 0, skybox_mesh.vertex_count);
-
-	glDisableVertexAttribArray(2);
-	glDisableVertexAttribArray(1);
-	glDisableVertexAttribArray(0);
-
-	glDepthMask(GL_TRUE);
-
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-}
-
-void init_preprocess(int width, int height) {
-	stages[0].shader = rafgl_program_create_from_name("pipeline/preprocess");
+void init_geometry(int width, int height) {
+	stages[0].shader = rafgl_program_create_from_name("geometry");
 	stages[0].uni[0] = glGetUniformLocation(stages[0].shader, "uni_m");
 	stages[0].uni[1] = glGetUniformLocation(stages[0].shader, "uni_v");
 	stages[0].uni[2] = glGetUniformLocation(stages[0].shader, "uni_p");
@@ -163,7 +118,7 @@ void init_preprocess(int width, int height) {
 	glDrawBuffers(3, buffers);
 }
 
-void render_preprocess() {
+void render_geometry() {
 	glBindFramebuffer(GL_FRAMEBUFFER, stages[0].fbo.fbo_id);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -188,7 +143,7 @@ void render_preprocess() {
 }
 
 void init_ssao(int width, int height) {
-	stages[1].shader = rafgl_program_create_from_name("pipeline/ssao");
+	stages[1].shader = rafgl_program_create_from_name("ssao");
 	stages[1].uni[0] = glGetUniformLocation(stages[1].shader, "uni_m");
 	stages[1].uni[1] = glGetUniformLocation(stages[1].shader, "uni_v");
 	stages[1].uni[2] = glGetUniformLocation(stages[1].shader, "uni_p");
@@ -222,9 +177,9 @@ void init_ssao(int width, int height) {
 		noise[i][2] = 0.0f;
 	}
 
-	glGenTextures(1, &smp_noise);
-	glBindTexture(GL_TEXTURE_2D, smp_noise);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 4, 4, 0, GL_RGB, GL_FLOAT, noise);
+	glGenTextures(1, &tex_noise);
+	glBindTexture(GL_TEXTURE_2D, tex_noise);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, 4, 4, 0, GL_RGB, GL_FLOAT, noise);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -238,9 +193,9 @@ void init_ssao(int width, int height) {
 		kernel[i][2] = ((float) rand()) / RAND_MAX;
 	}
 
-	glGenTextures(1, &smp_kernel);
-	glBindTexture(GL_TEXTURE_2D, smp_kernel);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 8, 8, 0, GL_RGB, GL_FLOAT, kernel);
+	glGenTextures(1, &tex_kernel);
+	glBindTexture(GL_TEXTURE_2D, tex_kernel);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, 8, 8, 0, GL_RGB, GL_FLOAT, kernel);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -272,7 +227,7 @@ void render_ssao() {
 }
 
 void init_blur(int width, int height) {
-	stages[2].shader = rafgl_program_create_from_name("pipeline/blur");
+	stages[2].shader = rafgl_program_create_from_name("blur");
 	stages[2].uni[0] = glGetUniformLocation(stages[2].shader, "uni_m");
 	stages[2].uni[1] = glGetUniformLocation(stages[2].shader, "uni_v");
 	stages[2].uni[2] = glGetUniformLocation(stages[2].shader, "uni_p");
@@ -309,7 +264,7 @@ void render_blur() {
 }
 
 void init_light(int width, int height) {
-	stages[3].shader = rafgl_program_create_from_name("pipeline/light");
+	stages[3].shader = rafgl_program_create_from_name("light");
 	stages[3].uni[0] = glGetUniformLocation(stages[3].shader, "uni_m");
 	stages[3].uni[1] = glGetUniformLocation(stages[3].shader, "uni_v");
 	stages[3].uni[2] = glGetUniformLocation(stages[3].shader, "uni_p");
@@ -360,35 +315,6 @@ void render_light() {
 	render_quad();
 }
 
-void init_postprocess(int width, int height) {
-	stages[4].shader = rafgl_program_create_from_name("pipeline/postprocess");
-	stages[4].uni[0] = glGetUniformLocation(stages[4].shader, "uni_m");
-	stages[4].uni[1] = glGetUniformLocation(stages[4].shader, "uni_v");
-	stages[4].uni[2] = glGetUniformLocation(stages[4].shader, "uni_p");
-	stages[4].fbo = rafgl_framebuffer_simple_create(width, height);
-
-	glBindFramebuffer(GL_FRAMEBUFFER, stages[4].fbo.fbo_id);
-
-	smp_light = glGetUniformLocation(stages[4].shader, "smp_light");
-
-	glUseProgram(stages[4].shader);
-	glUniform1i(smp_light, 0);
-}
-
-void render_postprocess() {
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-	glUseProgram(stages[4].shader);
-	glUniformMatrix4fv(stages[4].uni[0], 1, GL_FALSE, (void*) model.m);
-	glUniformMatrix4fv(stages[4].uni[1], 1, GL_FALSE, (void*) view.m);
-	glUniformMatrix4fv(stages[4].uni[2], 1, GL_FALSE, (void*) projection.m);
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, tex_light);
-
-	//render_quad();
-}
-
 void main_state_init(GLFWwindow* window, void* args, int width, int height) {
 	srand(time(NULL));
 
@@ -399,13 +325,12 @@ void main_state_init(GLFWwindow* window, void* args, int width, int height) {
 		rafgl_meshPUN_load_from_OBJ(meshes + i, mesh_names[i]);
 	}
 
-	init_skybox();
-	init_preprocess(width, height);
+	init_geometry(width, height);
 	init_ssao(width, height);
 	init_blur(width, height);
 	init_light(width, height);
-	init_postprocess(width, height);
 
+	glEnable(GL_DEPTH_TEST);
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
@@ -453,9 +378,9 @@ void main_state_update(GLFWwindow* window, float delta_time, rafgl_game_data_t* 
 
 	int i;
 
-	for(i = 0; i < TOTAL_STAGES; i++) {
-		if(current_shader != i && game_data->keys_pressed[RAFGL_KEY_0 + i + 1]) {
-			current_shader = i;
+	for(i = 0; i < 9; i++) {
+		if(current_texture != i && game_data->keys_pressed[RAFGL_KEY_0 + i + 1]) {
+			current_texture = i;
 		}
 	}
 
@@ -471,35 +396,43 @@ void main_state_update(GLFWwindow* window, float delta_time, rafgl_game_data_t* 
 }
 
 void main_state_render(GLFWwindow* window, void* args) {
-	render_skybox();
-	render_preprocess();
+	render_geometry();
 	render_ssao();
 	render_blur();
 	render_light();
-	render_postprocess();
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 	rafgl_texture_t tex;
 
-	switch(current_shader) {
-	case 0:
-		tex.tex_id = tex_color;
-		rafgl_texture_show(&tex, 1);
-		break;
-	case 1:
-		tex.tex_id = tex_ssao;
-		rafgl_texture_show(&tex, 1);
-		break;
-	case 2:
-		tex.tex_id = tex_blur;
-		rafgl_texture_show(&tex, 1);
-		break;
-	case 3:
-		tex.tex_id = tex_light;
-		rafgl_texture_show(&tex, 1);
-		break;
+	switch(current_texture) {
+		case 0:
+			tex.tex_id = tex_position;
+			break;
+		case 1:
+			tex.tex_id = tex_normal;
+			break;
+		case 2:
+			tex.tex_id = tex_color;
+			break;
+		case 3:
+			tex.tex_id = tex_noise;
+			break;
+		case 4:
+			tex.tex_id = tex_kernel;
+			break;
+		case 5:
+			tex.tex_id = tex_ssao;
+			break;
+		case 6:
+			tex.tex_id = tex_blur;
+			break;
+		case 7:
+			tex.tex_id = tex_light;
+			break;
 	}
+	
+	rafgl_texture_show(&tex, 1);
 }
 
 void main_state_cleanup(GLFWwindow* window, void* args) {
